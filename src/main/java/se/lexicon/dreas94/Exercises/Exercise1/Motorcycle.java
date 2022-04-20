d157bb6ac39eeb92a8845e3a7a94a7521b51c8dd
Andreas Eriksson <dreas94@gmail.com>
2022-04-20 16:34:41 +0200

    Finished Motorcycle

--- src/main/java/se/lexicon/dreas94/Exercises/Exercise1/Motorcycle.java
+++ src/main/java/se/lexicon/dreas94/Exercises/Exercise1/Motorcycle.java
@@ -2,4 +2,28 @@ package se.lexicon.dreas94.Exercises.Exercise1;
 
 public class Motorcycle extends Vehicle
 {
+    String motorCycleType;
+
+    public String getMotorCycleType()
+    {
+        return motorCycleType;
+    }
+
+    public void setMotorCycleType(String motorCycleType)
+    {
+        this.motorCycleType = motorCycleType;
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString() + "Motorcycle{" +
+                "motorCycleType='" + motorCycleType + '\'' +
+                '}';
+    }
+
+    public void drive()
+    {
+        System.out.println("Motorcycle is driving, is a " + motorCycleType);
+    }
 }
