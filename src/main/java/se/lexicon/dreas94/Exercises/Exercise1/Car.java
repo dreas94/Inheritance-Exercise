53d97a2d690ef2560ef3a3947025af30bfb6a83d
Andreas Eriksson <dreas94@gmail.com>
2022-04-20 15:56:36 +0200

    Started Exercise 1

---
+++ src/main/java/se/lexicon/dreas94/Exercises/Exercise1/Car.java
@@ -0,0 +1,5 @@
+package se.lexicon.dreas94.Exercises.Exercise1;
+
+public class Car extends Vehicle
+{
+}
--- src/main/java/se/lexicon/dreas94/Exercises/Exercise1/Vehicle.java
+++ src/main/java/se/lexicon/dreas94/Exercises/Exercise1/Vehicle.java
@@ -2,4 +2,47 @@ package se.lexicon.dreas94.Exercises.Exercise1;
 
 public class Vehicle
 {
+    private int vehicleId;
+    String registrationNumber;
+    String brand;
+
+    public int getVehicleId()
+    {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId)
+    {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getRegistrationNumber()
+    {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber)
+    {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getBrand()
+    {
+        return brand;
+    }
+
+    public void setBrand(String brand)
+    {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Vehicle{" +
+                "vehicleId=" + vehicleId +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
 }
